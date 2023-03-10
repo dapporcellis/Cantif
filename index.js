5ad77ba7c01c94f4d9b81dbeacb80ab27d55b8fb
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2023-03-10 15:53:36 -0300

    arrumado

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const path = require('path');
 
 app.use(express.urlencoded({ extended:true }));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/', routes)
 
